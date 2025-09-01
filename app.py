455150cb5f6b8f680255710f291e7f98bd6704f4
Fern Smith <USER_EMAIL>
2025-09-01 12:36:06 -0400

    update port

--- app.py
+++ app.py
@@ -1525,7 +1525,6 @@ if __name__ == '__main__':
     app.run(
         debug=True,
         host='0.0.0.0',
-        port=int(os.environ.get('PORT', 8080)),
-        use_reloader=False
+        port=8080
     )
 
